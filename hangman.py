00490d67f728ae51d176ef62172c953d29a1d9d1
jayto <119763326+jaytoEX@users.noreply.github.com>
2023-04-22 15:25:24 -0700

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,44 @@
+"""lives = 6
+chosen_word = "banana"
+guesses = {"G","Y","A"}
+word = ["_","A","_","_","_","_"]
+
+- word is chosen
+- write dashes "_" in place of the letters in the chosen word
+- start guessing letters
+- if letter isn not in chosen wod lose a life
+- if letter is in the chosen word replace the dashes where the letters apear
+- game finishes when there is no "_" or run out of lives"""
+
+lives = 6
+chosen_word = "banana"
+len_word = len(chosen_word)
+word = ["_"]*len_word
+print(chosen_word)
+
+
+while True: 
+    print(word)
+    letter = input("Guess a letter: ")
+    if letter.isalpha() and len(letter)==1:
+        if letter in chosen_word:
+            for x in range(len_word):
+                if letter==chosen_word[x]:
+                    word[x] = letter
+          # replace the "_" with the letter
+        else:
+            lives = lives - 1
+            print("lives:", lives)
+        # lose a life
+    else: 
+        lives = lives - 1
+        print("lives:", lives)
+    # lose a life
+    if lives==0:
+        print("Game over pleb")
+        break
+    if not "_" in word:
+        print("You've won champ:", chosen_word)
+        break
+
+
